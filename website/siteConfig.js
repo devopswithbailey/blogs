eae04ad4c1ac138ab48f7cb6277cff84b47bf349
xhuang4 <xiaofei.huang@optum.com>
2020-08-24 17:11:28 -0400

    deploy on github

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -54,9 +54,9 @@ const siteConfig = {
   users,
 
   /* path to images for header/footer */
-  // headerIcon: 'img/favicon.ico',
-  // footerIcon: 'img/favicon.ico',
-  // favicon: 'img/favicon.ico',
+  headerIcon: 'img/favicon.ico',
+  footerIcon: 'img/favicon.ico',
+  favicon: 'img/favicon.ico',
 
   /* Colors for website */
   colors: {
