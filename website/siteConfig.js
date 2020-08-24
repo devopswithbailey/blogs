b9f2e623db98f60235a4b143218a665dbac3639d
xhuang4 <xiaofei.huang@optum.com>
2020-08-24 16:19:27 -0400

    deploy on github

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -35,7 +35,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'devopswithbailey',
+  projectName: 'blog-v1',
   organizationName: 'devopswithbailey',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
