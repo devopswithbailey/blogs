7719ccc241b36386acf33274c3e1005e583908a7
xhuang4 <xiaofei.huang@optum.com>
2020-08-25 17:51:00 -0400

    deploy on github

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -28,8 +28,8 @@ const siteConfig = {
 
   title: 'DevOps with Bailey', // Title for your website.
   tagline: 'Blog for DevOps',
-  url: '/', // Your website URL
-  baseUrl: '', // Base URL for your project */
+  url: 'https://devopswithbailey.com', // Your website URL
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
